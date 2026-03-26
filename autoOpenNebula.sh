97b8f2dc15d58cb3f89ef2f55a231a6c33f4cc3c
Gabriele Faletra <faletragabriele@gmail.com>
2026-03-26 23:29:42 +0100

    first template

---
+++ autoOpenNebula.sh
@@ -0,0 +1,10 @@
+wget -4 -O- https://downloads.opennebula.io/repo/repo2.key | sudo gpg --dearmor --yes --output /etc/apt/trusted.gpg.d/opennebula.gpg
+echo "deb https://downloads.opennebula.io/repo/7.0/Ubuntu/24.04 stable opennebula" | sudo tee /etc/apt/sources.list.d/opennebula.list
+sudo apt update
+sudo apt install opennebula opennebula-fireedge opennebula-guacd -y
+sudo apt install opennebula-node-kvm -y
+sudo -u oneadmin ssh-keygen -t rsa -N "" -f /var/lib/one/.ssh/id_rsa
+sudo -u oneadmin cp /var/lib/one/.ssh/id_rsa.pub /var/lib/one/.ssh/authorized_keys
+sudo systemctl enable --now opennebula opennebula-fireedge
+sudo ufw allow 2616
+sudo ufw reload
