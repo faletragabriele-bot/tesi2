dcfc7064c710ea806a5a9350c3919710bcd0e076
Gabriele Faletra <faletragabriele@gmail.com>
2026-03-27 01:03:42 +0100

    risolti problemi con sudo

--- autoOpenNebula.sh
+++ autoOpenNebula.sh
@@ -1,9 +1,9 @@
 wget -4 -O- https://downloads.opennebula.io/repo/repo2.key | sudo gpg --dearmor --yes --output /etc/apt/trusted.gpg.d/opennebula.gpg
 echo "deb https://downloads.opennebula.io/repo/7.0/Ubuntu/24.04 stable opennebula" | sudo tee /etc/apt/sources.list.d/opennebula.list
-sudo apt update
-sudo apt install opennebula opennebula-fireedge opennebula-guacd -y
-sudo apt install opennebula-node-kvm -y
+apt update
+apt install opennebula opennebula-fireedge opennebula-guacd -y
+apt install opennebula-node-kvm -y
 sudo -u oneadmin ssh-keygen -t rsa -N "" -f /var/lib/one/.ssh/id_rsa
 sudo -u oneadmin cp /var/lib/one/.ssh/id_rsa.pub /var/lib/one/.ssh/authorized_keys
-sudo systemctl enable --now opennebula opennebula-fireedge
-sudo apt update && sudo apt upgrade -y && sudo autoremove -y
+systemctl enable --now opennebula opennebula-fireedge
+apt update && apt upgrade -y && apt autoremove -y
